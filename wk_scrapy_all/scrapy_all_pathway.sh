458ae0a4cdb8eb68b2fbfce0d2d78fdcc9cc7c8c
Chenbin Mason <chenbin2018@protonmail.com>
2019-04-06 10:30:13 +0800

    Update and rename scrapy_all.sh to scrapy_all_pathway.sh

--- wk_scrapy_all/scrapy_all_pathway.sh
+++ wk_scrapy_all/scrapy_all_pathway.sh
@@ -6,5 +6,5 @@ set -eu -i pipefail
 curl -o br08601.keg \
 "https://www.genome.jp/kegg-bin/download_htext?htext=br08601.keg&format=htext&filedir="
 
-grep "^E" br08601.keg | awk '{print $2}' |
-xargs -i -n 100 Pathway Get &> get_pathway_$(date +"%Y%m%d").log
+grep "^E" br08601.keg | awk '$2!="" {print $2}' |
+xargs -i -n 100 Pathway Get &> scrapy_all_pathway_$(date +"%Y%m%d").log
