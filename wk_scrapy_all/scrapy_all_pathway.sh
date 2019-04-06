bb598c5f7ef025b244b563f7b36f324a0f945318
Chenbin Mason <chenbin2018@protonmail.com>
2019-04-06 10:37:54 +0800

    Update scrapy_all_pathway.sh

--- wk_scrapy_all/scrapy_all_pathway.sh
+++ wk_scrapy_all/scrapy_all_pathway.sh
@@ -6,5 +6,8 @@ set -eu -i pipefail
 curl -o br08601.keg \
 "https://www.genome.jp/kegg-bin/download_htext?htext=br08601.keg&format=htext&filedir="
 
+curl -o br08610.keg \
+"https://www.genome.jp/kegg-bin/download_htext?htext=br08610.keg&format=htext&filedir="
+
 grep "^E" br08601.keg | awk '$2!="" {print $2}' |
 xargs -i -n 100 Pathway Get &> scrapy_all_pathway_$(date +"%Y%m%d").log
